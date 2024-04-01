20a4b16aefef44090c480b2ad8c34b7f81b4dd69
nate <70911747+nathanallen242@users.noreply.github.com>
2024-04-01 12:26:30 -0400

    mapbox library added; no token

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,18 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Map from 'react-map-gl';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const mapboxAccessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
+function App() {
   return (
-    <>
-      <div>
-        PLACEHOLDER
-      </div>
-    </>
-  )
+    <Map
+      mapboxAccessToken={mapboxAccessToken}
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14
+      }}
+      style={{width: 600, height: 400}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
+  );
 }
 
 export default App
