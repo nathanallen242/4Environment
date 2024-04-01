4120e553eb33cd7535c0cfee73e782f71873a504
moyo-ayy <moyoayoade@gmail.com>
2024-04-01 12:57:39 -0400

    slight ui change

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,20 +4,20 @@ import './App.css'
 
 const mapboxAccessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
-console.log(mapboxAccessToken);
-
 function App() {
   return (
-    <Map
+    <div className='mapbox'>
+      <Map
       mapboxAccessToken={mapboxAccessToken}
       initialViewState={{
         longitude: -81.7602544,
         latitude: 27.9944024,
         zoom: 6
       }}
-      style={{ width: '100vw', height: 'calc(100vh - 20px)' }} // Example with 20px subtracted from the height for any top UI elements
+      style={{ width: '100vw', height: 'calc(100vh - 50px)' }} // Example with 20px subtracted from the height for any top UI elements
       mapStyle="mapbox://styles/mapbox/satellite-streets-v12"
     />
+    </div>
   );
 }
 
