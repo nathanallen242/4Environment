6d013309e0eae4e28cb0223effe7ededd53c2f57
nate <70911747+nathanallen242@users.noreply.github.com>
2024-04-01 12:44:23 -0400

    env import fix; css in progress

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,17 +4,19 @@ import './App.css'
 
 const mapboxAccessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
+console.log(mapboxAccessToken);
+
 function App() {
   return (
     <Map
       mapboxAccessToken={mapboxAccessToken}
       initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
+        longitude: -81.7602544,
+        latitude: 27.9944024,
+        zoom: 6
       }}
-      style={{width: 600, height: 400}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
+      style={{ width: '100vw', height: 'calc(100vh - 20px)' }} // Example with 20px subtracted from the height for any top UI elements
+      mapStyle="mapbox://styles/mapbox/satellite-streets-v12"
     />
   );
 }
